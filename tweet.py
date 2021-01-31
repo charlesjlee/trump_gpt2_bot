ebc0a24429b829a89621f246377f4ab516fb6bb5
Charles Lee <charleslee592@gmail.com>
2021-01-30 21:36:01 -0500

    first working version

--- tweet.py
+++ tweet.py
@@ -2,10 +2,9 @@ import tweepy
 import os
 import re
 import sys
-import os
 import time
-from pprint import pprint
 from aitextgen import aitextgen
+
 import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
@@ -33,22 +32,19 @@ try:
 except FileNotFoundError:
     df = pd.DataFrame(columns=columns)
 
-# TODO: change `count` to 1
 # get latest tweet by followers
 follower_tweets = [
-    # (tweet.id_str, re.sub(r'http\S+', '', tweet.full_text).strip()) # TODO: reapply regex
     (tweet.id_str, tweet.full_text)
     for friend_id in api.friends_ids()
     for tweet in api.user_timeline(
         tweet_mode='extended',
         user_id=friend_id,
-        count=3,
+        count=1,
         include_rts=False,
         exclude_replies=True,
     )
 ]
-print("follower_tweets")
-pprint(follower_tweets)
+print(f"follower_tweets\n{follower_tweets}\n\n")
 
 # get first new, unprocessed tweet
 new_tweet = next(filter(lambda x: int(x[0]) not in set(df.prompt_tweet_id), follower_tweets), None)
@@ -57,27 +53,70 @@ print(f"{new_tweet=}")
 if not new_tweet:
     sys.exit("no new, unprocessed tweets. Aborting!")
 
-# # run through GPT2
-# start_time = time.time()
-# ai = aitextgen()
-# print(f"loaded model in {round(time.time()-start_time, 2)} seconds")
+# run through GPT2
+start_time = time.time()
+ai = aitextgen()
+print(f"loaded model in {round(time.time()-start_time, 2)} seconds")
 
-# start_time = time.time()
-# ai.generate(n=3, prompt="I believe in unicorns because", max_length=100)
-# answers = []
-# print(f"generated {len(answers)} results in {round(time.time()-start_time, 2)} seconds")
-response_tweet = 'xxxxxxxxxxx'
+prompt = re.sub(r'http\S+', '', new_tweet[1]).strip() # no image or video links
+t1 = "Immigration reform is fine but don't rush to give away our country! Sounds like that's what's happening."
+t2 = "The Republicans will get zero credit for passing immigration reform and I said zero!"
+t3 = "Randy Moss should not be bragging about himself I'm the only one who is allowed to do that!"
+prompt_updated = f"{prompt} Trump {t1} Trump {t2} Trump {t3} Trump I {prompt}" # magic sauce
+
+def process(s):
+    s = s.replace(prompt_updated,'').strip()
+    s = re.sub(r'http\S+', '', s).strip()
+
+    # strip first and last sentence
+    s = s[s.find('.')+1:s.rfind('.')+1].strip()
+    if len(s) > 280: s = s[:s.rfind('.')+1].strip()
+    if len(s) > 280: s = s[:s.rfind('.')+1].strip()
+
+    return s
+
+# ask GPT-2 small model to generate answers
+answers = list(map(process, ai.generate(n=30, max_length=220, prompt=prompt_updated, return_as_list=True)))
+print(f"generated {len(answers)} results in {round(time.time()-start_time, 2)} seconds")
+
+def jaccard_similarity(a, b):
+    x, y = set(a), set(b)
+    return len(x&y) / len(x|y)
+
+def score(row):
+    if 0<=row.len<10 or row.trump or row.symbols > 2: return 0
+    return row.jaccard + row.self_similarity
+
+# compute score for each answer
+data = {
+    'text': answers,
+    'len': list(map(len,answers)),
+    'trump': [s.lower().count("trump") for s in answers],
+    'symbols': [sum(ord(c)>=128 for c in s) for s in answers],
+    'jaccard': [1-sum(jaccard_similarity(a,b) for b in [t1,t2,t3])/3 for a in answers],
+    'self_similarity': [len(set(s))/len(s) if len(s) else 1 for s in answers],
+}
+df = pd.DataFrame(data)
+df['score'] = df.apply(score, axis=1)
+df.sort_values(by='score', ascending=False, inplace=True)
+
+print(f"\nTop 10 answers by score\n{42*'-'}\n{df.head(10)}\n")
+
+# choose randomly from score range of [0.4, 0.65]
+# shorter lengths score too high
+response = df[(0.4<df.score) & (df.score <0.65)].sample()
+print(f"Randomly selected response: {response}\n")
 
 # tweet as a reply
 try:
-    status = api.update_status(status=response_tweet,
-                               in_reply_to_status_id=new_tweet[0],
-                               auto_populate_reply_metadata=True)
+    status = api.update_status(status=response.text,
+                                in_reply_to_status_id=new_tweet[0],
+                                auto_populate_reply_metadata=True)
 except Exception as e:
     sys.exit(f"Failed to tweet as reply because {type(e).__name__} occurred. Arguments:\n{e.args}")
 
 # log tweet
-new_df = pd.DataFrame([[*new_tweet, status.id_str, response_tweet]], columns=columns)
+new_df = pd.DataFrame([[*new_tweet, status.id_str, response.text]], columns=columns)
 new_df.to_csv(FILE_PATH, mode='a', header=not os.path.exists(FILE_PATH), index=False, encoding='utf-8')
 
 print(f"appended new row to {FILE_PATH}:\n{new_df}")
