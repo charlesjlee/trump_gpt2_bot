f1e59ebab35ca6befee05661737e5ccb6e1e9760
Charles Lee <charleslee592@gmail.com>
2022-02-26 19:18:05 -0500

    refactor into functions

--- tweet.py
+++ tweet.py
@@ -3,10 +3,10 @@ import os
 import re
 import sys
 import time
-from requests.exceptions import ChunkedEncodingError
 from string import punctuation
 from collections import Counter
 from aitextgen import aitextgen
+from tenacity import retry, wait_exponential, stop_after_attempt
 
 import pandas as pd
 pd.set_option("display.max_columns", None)
@@ -18,66 +18,48 @@ consumer_secret = os.environ["consumer_secret"]
 access_token = os.environ["access_token"]
 access_token_secret = os.environ["access_token_secret"]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
 FILE_PATH = 'processed.csv'
 COLUMNS = ['prompt_tweet_id', 'prompt_tweet', 'response_tweet_id', 'response_tweet']
 BANNED_WORDS = ['hitler', 'kill']
 
-# load processed tweets
-try:
-    df = pd.read_csv(FILE_PATH, encoding='utf-8')
-except FileNotFoundError:
-    df = pd.DataFrame(columns=COLUMNS)
+PROMPT_FLAVOR_1 = "Immigration reform is fine but don't rush to give away our country! Sounds like that's what's happening."
+PROMPT_FLAVOR_2 = "The Republicans will get zero credit for passing immigration reform and I said zero!"
+PROMPT_FLAVOR_3 = "Randy Moss should not be bragging about himself I'm the only one who is allowed to do that!"
 
-# get latest tweet by followers
-try:
+def load_processed_tweets():
+    try:
+        return pd.read_csv(FILE_PATH, encoding='utf-8')
+    except FileNotFoundError:
+        return pd.DataFrame(columns=COLUMNS)
+
+@retry(wait=wait_exponential(multiplier=1, min=1, max=60), stop=stop_after_attempt(5))
+def get_latest_tweets_by_followers(api, count=10):
     follower_tweets = [
         (tweet.id_str, tweet.full_text)
         for friend_id in api.get_friend_ids()
         for tweet in api.user_timeline(
             tweet_mode='extended',
             user_id=friend_id,
-            count=10,
+            count=count,
             include_rts=False,
             exclude_replies=True,
         )
     ]
-except Exception as e:
-    sys.exit(f"Failed to get follower's tweets because {type(e).__name__} occurred. Arguments:\n{e.args}")
-print(f"follower_tweets\n{follower_tweets}\n\n")
+    print(f"follower_tweets\n{follower_tweets}\n\n")
+    return follower_tweets
 
-# get first new, unprocessed tweet
-new_tweet = next(filter(lambda x: int(x[0]) not in set(df.prompt_tweet_id), follower_tweets), None)
-print(f"{new_tweet=}")
-
-if not new_tweet:
-    print(42*'-' + '\nNo new, unprocessed tweets. Aborting!')
-else:
+@retry(wait=wait_exponential(multiplier=1, min=1, max=60), stop=stop_after_attempt(5))
+def load_aitextgen():
     # run through GPT-2 small model
     start_time = time.time()
 
-    def get_aitextgen(tries=5):
-        for n in range(tries):
-            try:
-                return aitextgen()
-            except ChunkedEncodingError as e:
-                if n == tries - 1:
-                    raise e
+    ai = aitextgen()
+    print(f"loaded aitextgen model in {round(time.time()-start_time, 2)} seconds")
+    return ai
 
-    try:
-        ai = get_aitextgen()
-        print(f"loaded model in {round(time.time()-start_time, 2)} seconds")
-    except Exception as e:
-        sys.exit(f"Failed to load aitextgen package because {type(e).__name__} occurred. Arguments:\n{e.args}")
-    
-    prompt = re.sub(r'http\S+', '', new_tweet[1]).strip() # no image or video links
-    t1 = "Immigration reform is fine but don't rush to give away our country! Sounds like that's what's happening."
-    t2 = "The Republicans will get zero credit for passing immigration reform and I said zero!"
-    t3 = "Randy Moss should not be bragging about himself I'm the only one who is allowed to do that!"
-    prompt_updated = f"{prompt} Trump {t1} Trump {t2} Trump {t3} Trump I {prompt}" # magic sauce
+def generate_candidate_tweets(ai, tweet_text):
+    prompt = re.sub(r'http\S+', '', tweet_text).strip() # no image or video links
+    prompt_updated = f"{prompt} Trump {PROMPT_FLAVOR_1} Trump {PROMPT_FLAVOR_2} Trump {PROMPT_FLAVOR_3} Trump I {prompt}" # magic sauce
     
     def process(s):
         s = s.replace(prompt_updated,'').strip()
@@ -90,41 +72,43 @@ else:
     
         return s
     
-    # generate answers
     start_time = time.time()
-    answers = list(map(process, ai.generate(n=60, max_length=220, prompt=prompt_updated, return_as_list=True)))
-    print(f"generated {len(answers)} results in {round(time.time()-start_time, 2)} seconds")
-    
-    def jaccard_similarity(a, b):
-        x, y = set(a), set(b)
-        return len(x&y) / len(x|y)
+    candidate_tweets = list(map(process, ai.generate(n=60, max_length=220, prompt=prompt_updated, return_as_list=True)))
+    print(f"generated {len(candidate_tweets)} results in {round(time.time()-start_time, 2)} seconds")
+    return candidate_tweets
 
-    def score(row):
-        if (row.len < 10 or row.len > 250 or
-            row.trump or row.symbols > 2 or
-            row.text[0] in punctuation or row.digits > 4 or
-            row.banned_words or row.repeated_sentences):
-            return 0
-        return row.jaccard + row.self_similarity
-    
-    # compute score for each answer
+def jaccard_similarity(a, b):
+    x, y = set(a), set(b)
+    return len(x&y) / len(x|y)
+
+def score(row):
+    if (row.len < 10 or row.len > 250 or
+        row.trump or row.symbols > 2 or
+        row.text[0] in punctuation or row.digits > 4 or
+        row.banned_words or row.repeated_sentences):
+        return 0
+    return row.jaccard + row.self_similarity
+
+def score_candidate_tweets(candidate_tweets):
     df = pd.DataFrame({
-        'text': answers,
-        'len': list(map(len,answers)),
-        'digits': [sum(map(str.isdigit, s)) for s in answers],
-        'trump': [s.lower().count("trump") for s in answers],
-        'symbols': [sum(ord(c)>=128 or c=='@' for c in s) for s in answers],
-        'banned_words': [sum(banned in s.lower() for banned in BANNED_WORDS) for s in answers],
-        'repeated_sentences': [sum(counter := Counter(map(str.strip, re.split(r"[.!?]", s))).values()) - len(counter) for s in answers],
-        'jaccard': [1-sum(jaccard_similarity(a,b) for b in [t1,t2,t3])/3 for a in answers],
-        'self_similarity': [len(set(s))/len(s) if len(s) else 1 for s in answers],
+        'text': candidate_tweets,
+        'len': list(map(len,candidate_tweets)),
+        'digits': [sum(map(str.isdigit, s)) for s in candidate_tweets],
+        'trump': [s.lower().count("trump") for s in candidate_tweets],
+        'symbols': [sum(ord(c)>=128 or c=='@' for c in s) for s in candidate_tweets],
+        'banned_words': [sum(banned in s.lower() for banned in BANNED_WORDS) for s in candidate_tweets],
+        'repeated_sentences': [sum(counter := Counter(map(str.strip, re.split(r"[.!?]", s))).values()) - len(counter) for s in candidate_tweets],
+        'jaccard': [1-sum(jaccard_similarity(a,b) for b in [PROMPT_FLAVOR_1,PROMPT_FLAVOR_2,PROMPT_FLAVOR_3])/3 for a in candidate_tweets],
+        'self_similarity': [len(set(s))/len(s) if len(s) else 1 for s in candidate_tweets],
     })
+
     df['score'] = df.apply(score, axis=1)
     df.sort_values(by='score', ascending=False, inplace=True)
-    print(f"\nTop 10 answers by score\n{42*'-'}\n{df.head(10)}\n")
-    
-    # choose randomly from score range of [0.4, 0.65]
-    # shorter lengths score too high
+    print(f"\nTop 10 candidate tweets by score\n{42*'-'}\n{df.head(10)}\n")
+
+    return df
+
+def choose_tweet_from_scored_candidates(df):
     filtered_df = df[(0.4 < df.score) & (df.score < 0.65)]
     if filtered_df.empty:
         print(42*'-' + '\nFailed to generate viable candidates. Aborting!')
@@ -132,17 +116,43 @@ else:
         response = filtered_df.sample()
         print(f"Randomly selected response: {response}\n")
 
-        # tweet as a reply
-        try:
-            status = api.update_status(status=response.text.item(),
-                                       in_reply_to_status_id=new_tweet[0],
-                                       auto_populate_reply_metadata=True)
-        except Exception as e:
-            sys.exit(f"Failed to tweet as reply because {type(e).__name__} occurred. Arguments:\n{e.args}")
+@retry(wait=wait_exponential(multiplier=1, min=1, max=60), stop=stop_after_attempt(5))
+def tweet_reply(api, follower_tweet_id, tweet):
+    return api.update_status(status=tweet,
+                             in_reply_to_status_id=follower_tweet_id,
+                             auto_populate_reply_metadata=True)
 
-        # log tweet
-        log_df = pd.DataFrame([[*new_tweet, status.id_str, response.text.item()]], columns=COLUMNS)
-        log_df.to_csv(FILE_PATH, mode='a', header=not os.path.exists(FILE_PATH), index=False, encoding='utf-8')
+if __name__ == "__main__":
+    df_processed_tweets = load_processed_tweets()
 
-        print(f"appended new row to {FILE_PATH}:\n{log_df}")
-        print(42*'-' + '\nScript succeeded!')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    follower_tweets = get_latest_tweets_by_followers(api)
+
+    # get first new, unprocessed tweet
+    new_follower_tweet = next(filter(lambda x: int(x[0]) not in set(df_processed_tweets.prompt_tweet_id), follower_tweets), None)
+    print(f"{new_follower_tweet=}")
+
+    if not new_follower_tweet:
+        print(42*'-' + '\nNo new, unprocessed tweets. Aborting!')
+        sys.exit(0)
+
+    ai = load_aitextgen()
+    candidate_tweets = generate_candidate_tweets(ai, new_follower_tweet[1])
+    df_scored_tweets = score_candidate_tweets(candidate_tweets)
+    new_tweet = choose_tweet_from_scored_candidates(df_scored_tweets)
+    print(f"{new_tweet=}")
+
+    if not new_tweet:
+        print(42*'-' + '\nFailed to generate viable candidates. Aborting!')
+        sys.exit(0)
+
+    status = tweet_reply(api, new_follower_tweet[0], new_tweet.text.item())
+
+    # log tweet
+    log_df = pd.DataFrame([[*new_tweet, status.id_str, new_tweet.text.item()]], columns=COLUMNS)
+    log_df.to_csv(FILE_PATH, mode='a', header=not os.path.exists(FILE_PATH), index=False, encoding='utf-8')
+
+    print(f"appended new row to {FILE_PATH}:\n{log_df}")
+    print(42*'-' + '\nScript succeeded!')
